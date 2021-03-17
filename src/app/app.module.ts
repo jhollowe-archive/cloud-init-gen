249ba3b566f557a85d5fabb6120b73957176868e
John Hollowell <johnahollowell@gmail.com>
2021-03-17 03:58:08 +0000

    Add stubs of main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './component/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfoComponent } from './component/info/info.component';
+import { EditorComponent } from './component/editor/editor.component';
+import { SelectorComponent } from './component/selector/selector.component';
+import { YamlComponent } from './component/yaml/yaml.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InfoComponent,
+    EditorComponent,
+    SelectorComponent,
+    YamlComponent
   ],
   imports: [
     BrowserModule,
